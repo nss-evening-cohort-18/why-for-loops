eb7ae259e5ef10facb5b32761ef9f3106c5a7d7e
David Yarbrough <yarbroughdavidc@gmail.com>
2022-01-29 12:40:32 -0600

    adding instructors database

---
+++ database.js
@@ -0,0 +1,20 @@
+const instructors = [
+    {
+        name: "David",
+        verve: 6.0,
+        moxie: 4.5,
+        jeNeSaisQuoi: yes
+    },
+    {
+        name: "April",
+        verve: 9.0,
+        moxie: 8.5,
+        jeNeSaisQuoi: yes
+    },
+    {
+        name: "John",
+        verve: 8.0,
+        moxie: 9.5,
+        jeNeSaisQuoi: yes
+    }
+]
